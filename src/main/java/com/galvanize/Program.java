be1e948787acf5e7d0c31b90f82637d341a3dd6d
Ryan Cowles <ryan.cowles3@gmail.com>
2019-06-01 17:40:41 -0700

    created build files

--- src/main/java/com/galvanize/Program.java
+++ src/main/java/com/galvanize/Program.java
@@ -2,6 +2,6 @@ package com.galvanize;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("It Works " + args[0] + args[1]);
+        System.out.println("It Works!");
     }
 }
